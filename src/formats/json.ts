79d6b7f56ef3419c9835f46cc21ce0f71a40b675
Danny Wexler <dannywexler@gmail.com>
2026-04-18 18:52:45 -0700

    added serializeError to stringifyJSON

--- src/formats/json.ts
+++ src/formats/json.ts
@@ -1,6 +1,7 @@
 import type { ParseError, ParseOptions } from "jsonc-parser"
 import { parse } from "jsonc-parser"
 import { configure } from "safe-stable-stringify"
+import { serializeError } from "serialize-error"
 
 // -------------- PARSE --------------
 
@@ -16,13 +17,17 @@ export function parseJSON(
     stringifiedContent: string,
     parseOptions?: ParseJSONOptions,
 ): unknown {
-    const mergedOptions = { ...defaultParseJSONOptions, ...parseOptions }
-    const errors: Array<ParseError> = []
-    const unknownContent = parse(stringifiedContent, errors, mergedOptions)
-    if (errors.length > 0) {
-        throw new SyntaxError("Could not parse JSON", { cause: errors })
+    try {
+        return JSON.parse(stringifiedContent)
+    } catch {
+        const mergedOptions = { ...defaultParseJSONOptions, ...parseOptions }
+        const errors: Array<ParseError> = []
+        const unknownContent = parse(stringifiedContent, errors, mergedOptions)
+        if (errors.length > 0) {
+            throw new SyntaxError("Could not parse JSON", { cause: errors })
+        }
+        return unknownContent
     }
-    return unknownContent
 }
 
 // -------------- STRINGIFY --------------
@@ -43,7 +48,10 @@ export function stringifyJSON(
     unknownContent: unknown,
     stringifyOptions?: StringifyJSONOptions,
 ) {
-    const res = configuredStringify(unknownContent, null, stringifyOptions)
+    const content = Error.isError(unknownContent)
+        ? serializeError(unknownContent)
+        : unknownContent
+    const res = configuredStringify(content, null, stringifyOptions)
     if (res === undefined) {
         throw new SyntaxError(
             "safe-stable-stringify should have thrown an error if trying to stringify an invalid json object",
